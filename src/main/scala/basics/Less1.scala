d09d96d7906360a0f97a61cf80bd89db9042aa0b
Artsemi.Kanaplianik <artemiy.konoplyanik@gmail.com>
2021-08-12 12:55:30 +0300

    fibonacci fix1

--- src/main/scala/basics/Less1.scala
+++ src/main/scala/basics/Less1.scala
@@ -1,18 +1,31 @@
 package basics
 
+import scala.annotation.tailrec
+
 object Less1 extends App {
 
   def fibonacci(n: Int): Either[String, Int] = {
+    @tailrec
+    def count(f0: Int = 0, f1: Int = 1, number: Int = 2): Int = {
+      number match {
+        case x if x == n => f0 + f1
+        case _ => count(f1, f0 + f1, number + 1)
+      }
+    }
     n match {
-      case x if x < 0 => Left("Error message")
-      case 0 => Right(0)
-      case 1 => Right(1)
-      case y if y > 1 => if (fibonacci(y-1).isRight && fibonacci(y-2).isRight) {
-        Right(fibonacci(y-1).getOrElse(-1) + fibonacci(y-2).getOrElse(-1))
-      } else Left("Super magic") // fix
-      case _ => Left("Magic here")
+      case x if x < 0 => Left("Error: negative numbers aren't allowed")
+      case x if x <= 1 => Right(x)
+      case _ => Right(count())
     }
   }
+
+//  //test
+//  val l = for {
+//    i <- (0 to 19).toList
+//  } yield fibonacci2(i).getOrElse(-1)
+//  println(l + " my list")
+//  val l2 = List(0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181)
+//  println(l2 + " true list")
 
   def atkinSieve(n: Int): List[Int] = {
     val sqrLim = Math.sqrt(n).toInt
@@ -36,12 +49,8 @@ object Less1 extends App {
 
   def delSqr(list: List[Int]): List[Int] = {
     list.filter(x => !list.exists(y => x / (y * y) >= 1 && x % (y * y) == 0))
-    list.filter(x => !list.exists(y => list.exists(z => x == y * z))) //???
+    list.filter(x => !list.exists(y => list.exists(z => x == y * z)))
   }
 
-  println(atkinSieve(200))
-  // 91 133 143
-  // 91 - 7 13
-  // 133 - 7 19
-  // 143 - 11 13
+//  println(atkinSieve(200))
 }
